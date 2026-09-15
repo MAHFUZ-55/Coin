01d8f97a83433ab34493e5b5e7ac8b1b3ecbf745
Md Mahfuz Hasan <127038327+MAHFUZ-55@users.noreply.github.com>
2026-09-15 06:00:20 +0600

    Add files via upload

---
+++ app/scr/main/java/com/example/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.ui.screens.MainAppScreen
+import com.example.ui.theme.MyApplicationTheme
+import com.example.ui.viewmodel.MhMiningViewModel
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel: MhMiningViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                MainAppScreen(
+                    viewModel = viewModel,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+}
+
